d6cdde955961c2841e8884f288d4c13bb67bc091
Andrey <hornespill@gmail.com>
2021-01-30 23:33:34 +0200

    react hooks update

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,69 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from './menu';
+import useProgressiveImage from './hooks/progressiveImg';
+
+/* *************************************************************
+|
+|
+|                      intro.js file
+|
+|     Intro file w/ react hooks style
+|     for better development and code organisation. 
+|
+|      
+|      
+|      More info: https://andrewburw.github.io/personalpage/
+|
+|
+| **************************************************************/
+
+const Intro = () => {
+  // ************************ /\ ****************************
+
+  const [menu, toggleMenu] = useState(false);
+  const src = useProgressiveImage({
+    src:
+      './images/cover.jpg',
+    fallbackSrc:
+      './images/cover-min.jpg',
+  });
 
 
-class Main extends React.Component {
-          state = {
-             openNav : false
-          }
-
-  openMenu = () => {
-
-    this.setState({openNav:true});
-  }        
-
-  closeMenu = () => {
-
-    this.setState({openNav:false})
-  }
-  render() {
-
-//  <Menu show={this.state.openNav} closeMenu={this.closeMenu} />
-
-
-    return (
-      <div>
-       <Menu show={this.state.openNav} closeMenu={this.closeMenu} />
+  return (
+    <div>
+      <Menu show={menu} closeMenu={toggleMenu} />
       <div className="flex_container">
 
-<nav className="nav">
-  <div className="nav__menu container">
+        <nav className="nav">
+          <div className="nav__menu container">
 
 
-    <div className="logo">
-      <p>A n d r e w</p>
+            <div className="logo">
+              <p>A n d r e w</p>
+            </div>
+
+            <div className="nav-burger__menu" style={{ "cursor": "pointer" }} onClick={toggleMenu}>
+
+              <div className="burger__line"></div>
+              <div className="burger__line"></div>
+              <div className="burger__line"></div>
+            </div>
+
+
+
+          </div>
+        </nav>
+
+
+        <main className="flex_item_left container">
+          <div className="main-slogan">
+            <h1>Simple is beautiful</h1>
+
+          </div>
+          <div className="old__version">
+            <a href="https://andrewburw.github.io/personal-page/">Old version.</a>
+          </div>
+
+        </main>
+
+        <div className={src === '../images/cover.jpg' ? 'flex_item_right  thumb' : 'flex_item_right  full'}
+          style={{ 'backgroundImage': 'url(' + src + ')' }}
+        >
+
+        </div>
+      </div>
     </div>
-
-      <div className="nav-burger__menu" style={{"cursor":"pointer"}} onClick={this.openMenu}>
-        
-        <div className="burger__line"></div>
-        <div className="burger__line"></div>
-        <div className="burger__line"></div>
-      </div>
-
-
-
-  </div>
-</nav>
-
-
-      <main className="flex_item_left container">
-      <div className="main-slogan">
-        <h1>Simple is beautiful</h1>
-
-      </div>
-       <div className="old__version">
-         <a href="https://andrewburw.github.io/personal-page/">Old version.</a>
-       </div>
-
-    </main>
-      <div className="flex_item_right">
-
-   </div>
- </div>
-      </div>
-    );
-  }
+  );
 }
 
-export default Main;
+
+export default Intro;
--- src/components/main.js
+++ src/components/main.js
@@ -1,100 +1,91 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import AboutPage from './aboutPage';
 import SkilzzPage from './skillzPage';
 import PortfolioPage from './portfolioPage';
 import ArtPage from './artPage';
 import ContactsPage from './contactsPage';
 import Menu from './menu';
-import useProgressiveImage from './hooks/progressiveImg'
+import useProgressiveImage from './hooks/progressiveImg';
 import {
- 
-  
-    Switch,
-    Route
-  } from "react-router-dom";
 
+  Switch,
+  Route
+} from "react-router-dom";
+/* *************************************************************
+|
+|
+|                      main.js file
+|
+|       Main page in react hooks style,
+|      
+|
+|   
+|      
+|      More info: https://andrewburw.github.io/personalpage/
+|
+|
+| **************************************************************/
 
- const Main  = () => {
-// ************************ /\ ****************************
+const Main = () => {
+  // ************************ /\ ****************************
 
-
+  const [menu, toggleMenu] = useState(false);
   const src = useProgressiveImage({
     src:
       '../images/bg-cover2.jpg',
     fallbackSrc:
       '../images/bg-cover2-min.jpg',
   });
-  /* 
-    state = {
-        openNav : false
-     }
 
-openMenu = () => {
-
-this.setState({openNav:true});
-}        
-
-closeMenu = () => {
-
-this.setState({openNav:false})
-}
-*/
-    
-
-//    <Menu show={this.state.openNav} closeMenu={this.closeMenu} />
-//      <div className="nav-burger__menu" style={{"cursor":"pointer"}} onClick={this.openMenu}>
- 
-// crutches 
-
-return (
-      <div>
-     
-             <nav className="nav">
-       <div className="nav__menu container">
+  return (
+    <div>
+      <Menu show={menu} closeMenu={toggleMenu} />
+      <nav className="nav">
+        <div className="nav__menu container">
 
 
-    <div className="logo">
-    <p>A n d r e w</p>
-    </div>
+          <div className="logo">
+            <p>A n d r e w</p>
+          </div>
 
-      <div className="nav-burger__menu" style={{"cursor":"pointer"}} >
-        <div className="burger__line"></div>
-        <div className="burger__line"></div>
-        <div className="burger__line"></div>
+          <div className="nav-burger__menu" style={{ "cursor": "pointer" }} onClick={toggleMenu}>
+            <div className="burger__line"></div>
+            <div className="burger__line"></div>
+            <div className="burger__line"></div>
+          </div>
+
+
+
+        </div>
+      </nav>
+
+      <div className="background__image container">
+
+        <div className={src === '../images/bg-cover2-min.jpg' ? 'imagin thumb' : 'imagin full'}
+          style={{ 'backgroundImage': 'url(' + src + ')' }}
+        >
+        </div>
       </div>
 
 
-
-  </div>
-</nav>
-
-<div className="background__image container">
-   
-<div className={src === '../images/bg-cover2-min.jpg' ? 'imagin thumb':'imagin full '} 
-style={{'backgroundImage': 'url("../images/bg-cover2.jpg")'}} 
-src={src} > 
-</div>
-  </div>
-
- 
       <Switch>
-        <Route  path="/main/about" exact component={AboutPage} />
-        <Route  path="/main/skills" component={SkilzzPage} />
-        <Route  path="/main/portfolio" component={PortfolioPage} />
-        <Route  path="/main/art" component={ArtPage} />
-        <Route  path="/main/contacts" component={ContactsPage} />
+        <Route path="/main/about" exact component={AboutPage} />
+        <Route path="/main/skills" component={SkilzzPage} />
+        <Route path="/main/portfolio" component={PortfolioPage} />
+        <Route path="/main/art" component={ArtPage} />
+        <Route path="/main/contacts" component={ContactsPage} />
       </Switch>
-   
-     
-  <div className="footer">
-         <p>® 2020 Andrew. All Rights Reserved.</p>
+
+
+      <div className="footer">
+        <p>® 2020 Andrew. All Rights Reserved.</p>
       </div>
 
-      
-      </div>
-    );
-  }
+
+    </div>
+  );
+}
 
 
 export default Main;
